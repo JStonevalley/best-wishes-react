9179cb6b138c17b5b755dc3a6f8518965940112f
Jonas Stendahl <jonas.stendahl@outlook.com>
2020-05-17 10:43:12 +0200

    Use square images and use contain

--- src/workshop/components/Wish.jsx
+++ src/workshop/components/Wish.jsx
@@ -33,8 +33,8 @@ const styles = theme => ({
   image: {
     width: '100%',
     height: '100%',
-    objectFit: 'cover',
-    borderRadius: '50%'
+    objectFit: 'contain',
+    borderRadius: '5%'
   },
   imageContainer: {
     alignSelf: 'center',

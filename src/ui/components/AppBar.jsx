264a9cbfcb2588d4c6ab12a560095ee5e21c371a
Jonas Stendahl <jonas.stendahl@outlook.com>
2021-03-20 14:42:18 +0100

    Remove side menu

--- src/ui/components/AppBar.jsx
+++ src/ui/components/AppBar.jsx
@@ -1,20 +1,6 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  IconButton,
-  SwipeableDrawer,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText
-} from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu'
-import InboxIcon from '@material-ui/icons/MoveToInbox'
-import ListIcon from '@material-ui/icons/List'
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
@@ -33,68 +19,26 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ButtonAppBar() {
   const classes = useStyles()
-  const [drawerIsOpen, setDrawerIsOpen] = useState(false)
 
   return (
-    <>
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton
-            edge='start'
-            className={classes.menuButtonInAppBar}
-            color='inherit'
-            aria-label='menu'
-            onClick={() => setDrawerIsOpen(!drawerIsOpen)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant='h6' className={classes.title}>
-            Bestwishes
-          </Typography>
-          <Link
-            to='/login'
-            style={{ color: 'inherit', textDecoration: 'inherit' }}
-          >
-            <Button color='inherit'>Login</Button>
-          </Link>
-          <Link
-            to='/signup'
-            style={{ color: 'inherit', textDecoration: 'inherit' }}
-          >
-            <Button color='inherit'>Signup</Button>
-          </Link>
-        </Toolbar>
-      </AppBar>
-      <SwipeableDrawer
-        anchor='left'
-        open={drawerIsOpen}
-        onClose={() => setDrawerIsOpen(false)}
-        onOpen={() => setDrawerIsOpen(true)}
-      >
-        <IconButton
-          edge='start'
-          className={classes.menuButtonInMenu}
-          color='inherit'
-          aria-label='menu'
-          onClick={() => setDrawerIsOpen(!drawerIsOpen)}
+    <AppBar position='static'>
+      <Toolbar>
+        <Typography variant='h6' className={classes.title}>
+          Bestwishes
+        </Typography>
+        <Link
+          to='/login'
+          style={{ color: 'inherit', textDecoration: 'inherit' }}
         >
-          <MenuIcon />
-        </IconButton>
-        <List>
-          <ListItem button>
-            <ListItemIcon>
-              <ListIcon />
-            </ListItemIcon>
-            <ListItemText primary='My lists' />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary='Lists shared with me' />
-          </ListItem>
-        </List>
-      </SwipeableDrawer>
-    </>
+          <Button color='inherit'>Login</Button>
+        </Link>
+        <Link
+          to='/signup'
+          style={{ color: 'inherit', textDecoration: 'inherit' }}
+        >
+          <Button color='inherit'>Signup</Button>
+        </Link>
+      </Toolbar>
+    </AppBar>
   )
 }
